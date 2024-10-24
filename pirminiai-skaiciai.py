bbe2baa5cbeff9b7177bc4da39c4426e52cd6c8c
Karolis Paskevicius <kp704595@gmail.com>
2024-10-24 17:35:10 +0300

    Vartotojo ivesties surinkimas pirminiu skaiciu paieskos programoje

---
+++ pirminiai-skaiciai.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print("Pirminiu skaiciu paieskos programa.")
+print("Programa ras pirminius skaicius nurodytame intervale. ")
+skaicius_nuo = input("Iveskite intervalo pradzia: ")
+skaicius_iki = input("Iveskite intervalo pabaiga: ")
+print("Pirminiu skaiciu ieskoma intervale [" + str(skaicius_nuo) + ", " + str(skaicius_iki) + "]")
+
