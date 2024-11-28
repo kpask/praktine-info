7066a4a29bafde5738e9bcc3519225cfa3d70b89
Karolis Paskevicius <kp704595@gmail.com>
2024-11-29 00:23:23 +0200

    Mūro dėsnio grafiko braižymo pradinė versija

--- 09-uzduotis/moores-law.py
+++ 09-uzduotis/moores-law.py
@@ -1,14 +1,21 @@
+import matplotlib.pyplot
 
 f = open("transistor-counts.csv", "r", encoding="utf8")
+
 eiluciu_skaicius = 0
 procesoriuPavadinimai = []
 tranzistoriuSkaiciai = []
 isleidimoMetai = []
+
+#surenkami duomenys
 for eilute in f:
     eilute = eilute.strip().split(",")
     if eiluciu_skaicius > 0:
         procesoriuPavadinimai.append(eilute[0])
         tranzistoriuSkaiciai.append(int(eilute[1]))
         isleidimoMetai.append(int(eilute[2]))
+        print(eilute)
     eiluciu_skaicius += 1
-    print(eilute)
+
+#Braizome grafika
+matplotlib.pyplot.scatter(isleidimoMetai, tranzistoriuSkaiciai, label = "Tranzistoriu skaičius")
