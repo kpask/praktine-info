33c6d998aad37183067d95a9015b02544c7a5f6f
Karolis Paskevicius <kp704595@gmail.com>
2024-11-29 13:01:18 +0200

    Išsaugome gautą gauto grafiko failus

--- 09-uzduotis/moores-law.py
+++ 09-uzduotis/moores-law.py
@@ -22,17 +22,23 @@ for eilute in f:
 prognoze = []
 metu_intervalas = range(min(isleidimoMetai), max(isleidimoMetai)+1)
 
-# 2.1 Pagal Mūro dėsni paskaičiuojame kiekvienų metų prognozę
+# 2.1 Pagal Mūro dėsnį paskaičiuojame kiekvienų metų prognozę
 n_0 = min(tranzistoriuSkaiciai)
-for y_i in range(min(isleidimoMetai), max(isleidimoMetai)+1):
-    prognoze.append(n_0 * 2 ** ((y_i - min(isleidimoMetai))/2))
+for y_i in metu_intervalas:
+    prognoze.append(n_0 * 2 ** ((y_i - min(isleidimoMetai)) / 2))
 
-# 3. Braizome grafika
-matplotlib.pyplot.scatter(isleidimoMetai, tranzistoriuSkaiciai, label = "Sukurti procesoriai")
+# 3. Braižome grafiką
+matplotlib.pyplot.scatter(isleidimoMetai, tranzistoriuSkaiciai, label="Sukurti procesoriai")
 matplotlib.pyplot.plot(metu_intervalas, prognoze, label="Prognozė", color="gray", linestyle="--")
 matplotlib.pyplot.title("Mūro dėsnis")
 matplotlib.pyplot.xlabel("Išleidimo metai")
 matplotlib.pyplot.ylabel("Tranzistoriu skaičius")
 matplotlib.pyplot.yscale('log')
 matplotlib.pyplot.legend()
+
+# 4. Išsaugojame grafiką įvairiais formatais
+matplotlib.pyplot.savefig("muro_desnis.pdf", format="pdf")
+matplotlib.pyplot.savefig("muro_desnis.png", format="png")
+matplotlib.pyplot.savefig("muro_desnis.svg", format="svg")
+
 matplotlib.pyplot.show()
