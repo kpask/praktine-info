5d4899d9d59699334df71311c27c08ece603ba64
Karolis Paskevicius <kp704595@gmail.com>
2024-11-29 12:25:53 +0200

    Įdedame grafiko legendą

--- 09-uzduotis/moores-law.py
+++ 09-uzduotis/moores-law.py
@@ -9,7 +9,7 @@ isleidimoMetai = []
 
 # 1. surenkami duomenys
 for eilute in f:
-    # 1.1 Suskaldome stulpelius pagal kableli
+    # 1.1 Suskaldome CSV stulpelius, remiantis kableliu
     eilute = eilute.strip().split(",")
     if eiluciu_skaicius > 0:
         procesoriuPavadinimai.append(eilute[0])
@@ -18,22 +18,21 @@ for eilute in f:
         print(eilute)
     eiluciu_skaicius += 1
 
+# 2. Deklaruojami kintamieji Mūro prognozei atvaizduoti
 prognoze = []
 metu_intervalas = range(min(isleidimoMetai), max(isleidimoMetai)+1)
 
+# 2.1 Pagal Mūro dėsni paskaičiuojame kiekvienų metų prognozę
 n_0 = min(tranzistoriuSkaiciai)
 for y_i in range(min(isleidimoMetai), max(isleidimoMetai)+1):
     prognoze.append(n_0 * 2 ** ((y_i - min(isleidimoMetai))/2))
-# 2. Įtraukiame prognozes kiekvieniems metams
-
-print(len(isleidimoMetai))
-print(len(prognoze))
 
 # 3. Braizome grafika
-matplotlib.pyplot.scatter(isleidimoMetai, tranzistoriuSkaiciai, label = "Tranzistoriu skaičius")
-matplotlib.pyplot.plot(metu_intervalas, prognoze, label="Prognozė (Mūro dėsnis)", color="gray", linestyle="--")
+matplotlib.pyplot.scatter(isleidimoMetai, tranzistoriuSkaiciai, label = "Sukurti procesoriai")
+matplotlib.pyplot.plot(metu_intervalas, prognoze, label="Prognozė", color="gray", linestyle="--")
 matplotlib.pyplot.title("Mūro dėsnis")
 matplotlib.pyplot.xlabel("Išleidimo metai")
 matplotlib.pyplot.ylabel("Tranzistoriu skaičius")
 matplotlib.pyplot.yscale('log')
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
