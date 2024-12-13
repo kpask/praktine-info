c715e745db1c3bda41205e71f7bd329d86975f03
Karolis Paskevicius <kp704595@gmail.com>
2024-12-13 12:34:18 +0200

    2. Braižome 1/x^(1/3) grafiko trūkį ir padidiname skyrą

--- 10-uzduotis/funkciju-grafikai.py
+++ 10-uzduotis/funkciju-grafikai.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-X = np.linspace(-5, 5, 5000)
+X = np.linspace(-5, 5, 50001)
 F1  = 1/(np.cbrt(X))
 plt.plot(X, F1, linestyle="-", label=r"$f_1(x)=\frac{1}{\sqrt[3]{x}}$")
 plt.show()
