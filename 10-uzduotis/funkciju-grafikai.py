00a7c21557667d77d89abc247b71bd2a37cb884b
Karolis Paskevicius <kp704595@gmail.com>
2024-12-13 12:21:14 +0200

    1. 1/x^(1/3) grafiko braižymo pradinė versija

---
+++ 10-uzduotis/funkciju-grafikai.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+X = np.linspace(-5, 5, 5000)
+F1  = 1/(np.cbrt(X))
+plt.plot(X, F1, linestyle="-", label=r"$f_1(x)=\frac{1}{\sqrt[3]{x}}$")
+plt.show()
+plt.close()
